4851716c9da631c1bdb9c52dd6880702e1ec4c10
Prabakaran-M <mspk028@gmail.com>
2023-06-23 16:56:34 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
+import { useEffect, useState } from "react";
 import "./styles/App.css";
+import List from "./components/List";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // fetching api
+  const fetchApi = async () => {
+    try {
+      await fetch("https://reqres.in/api/users?page=2")
+        .then((response) => response.json())
+        .then((data) => setList(data.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  // search by name
+  const handleSearch = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+
+    if (query.length > 0) {
+      const filteredEmployees = list.filter((employee) =>
+        employee.first_name.toLowerCase().includes(query.toLowerCase())
+      );
+      setList(filteredEmployees);
+    } else {
+      fetchApi();
+    }
+  };
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="px-4 md:px-10 lg:px-20 py-10">
+      <h1 className="text-center font-semibold text-lg md:text-2xl mb-4">
+        INTERNSHIP ASSIGNMENT
+      </h1>
+      <div className="mt-5 mb-14">
+        <input
+          type="text"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-[95%] md:w-[400px] mx-auto"
+          placeholder="Search here..."
+          value={searchQuery}
+          onChange={handleSearch}
+        />
+      </div>
+      <List list={list} />
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const List = ({ list }) => {
+  return (
+    <div className="flex flex-col md:flex-row md:flex-wrap md:gap-8 lg:gap-10 gap-4 items-center justify-center">
+      {list.length > 0 ? (
+        <>
+          {list.map((data) => {
+            return (
+              <div
+                key={data.id}
+                className="flex flex-col gap-2 items-center justify-center"
+              >
+                <div className="relative rounded-3xl w-[300px] h-[300px] border-black border-[2px] p-6">
+                  <p className="absolute -right-3 -top-3 bg-black/90 border-black border-[3px] text-gray-300 rounded-full w-[40px] h-[40px] flex items-center justify-center ">
+                    {data.id}
+                  </p>
+                  <div className="m-auto flex items-center justify-center">
+                    <img
+                      src={data.avatar}
+                      alt="avatar"
+                      className="rounded-3xl h-full"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <p className="font-medium text-gray-600">{data.first_name}</p>
+                </div>
+              </div>
+            );
+          })}
+        </>
+      ) : (
+        <p className="text-xl md::text-3xl text-gray-500 font-semibold">
+          No employees found
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default List;
